2fde4ad0c14d4688570780bd6d772316aff39f1d
Aaron Kanefsky <aaronk03@vt.edu>
2023-11-03 10:46:07 -0400

    Added header messages

--- commands.py
+++ commands.py
@@ -1,4 +1,9 @@
+'''
+Define the functions/methods that we are going to use. This file
+is split up between the functions to format and send our own data
+and interpret the received data
 
+'''
 ###############################################################
 #-------------------------------------------------------------#
 #                         Send Methods                        #
