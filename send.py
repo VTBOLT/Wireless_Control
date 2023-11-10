38ae8948f1edbce547ce2b8a26e26d9e608d1de4
n1r0k <nrozenov42@vt.edu>
2023-11-10 15:39:53 -0500

    Updated Send Code

--- send.py
+++ send.py
@@ -7,7 +7,7 @@ import math
 import serial as ps
 import constants
 constants = constants.Constants # Check the constants as needed for testing
-com='Com9'
+com='COM9'
 
 transmittedString=input()
 transmittedStringList=list(transmittedString)
@@ -20,7 +20,7 @@ dataSum=0
 for i in range(len(asciiTransmittedStringList)):
     dataSum=dataSum+asciiTransmittedStringList[i]
 
-constantHexListInDec=[16,1,0,0,0,0,0,0,0,0,255,254,0,0]
+constantHexListInDec=[16,1,0,0,0,0,0,0,255,255,255,255,0,0]
 
 constantDecSum=0
 for i in range(len(constantHexListInDec)):
@@ -28,6 +28,7 @@ for i in range(len(constantHexListInDec)):
 
 hexTotalSumDec=dataSum+constantDecSum
 last2HexDec=hexTotalSumDec%pow(16,2)
+
 
 checkSumDec=255-last2HexDec
 transmitLength=len(asciiTransmittedStringList)+len(constantHexListInDec)
@@ -48,14 +49,19 @@ for i in range(len(constantHexListInDec)):
 for i in range(len(asciiTransmittedStringList)):
    hexTransmitLengthString=hexTransmitLengthString+" "+str(hex(asciiTransmittedStringList[i]))
 
+
+
 hexTransmitLengthString=hexTransmitLengthString+" "+str(hex(checkSumDec))
+print(hexTransmitLengthString)
 
-
+hexTransmitLengthString=hexTransmitLengthString+" "
 finalTransmit=""
 stringitr=0
-while stringitr<=len(hexTransmitLengthString):
-   hexTransmistSubstring=hexTransmitLengthString[stringitr+2:stringitr+4]
-   hexSubstringList=list(hexTransmistSubstring)
+transmitStringLength=len(hexTransmitLengthString)
+
+while stringitr<len(hexTransmitLengthString):
+   hexTransmitSubstring=hexTransmitLengthString[stringitr+2:stringitr+4]
+   hexSubstringList=list(hexTransmitSubstring)
    
    
    if hexSubstringList[len(hexSubstringList)-1]==" ":
@@ -66,15 +72,19 @@ while stringitr<=len(hexTransmitLengthString):
        finalTransmit=finalTransmit+str(hexSubstringList[t])
       finalTransmit=finalTransmit+" "
       stringitr=stringitr+4
+      transmitStringLength=transmitStringLength+1
    else:
-      finalTransmit=finalTransmit+hexTransmistSubstring+" "
+      finalTransmit=finalTransmit+hexTransmitSubstring+" "
       stringitr=stringitr+5
 
-
+finalTransmit=finalTransmit.upper()
+finalTransmit=finalTransmit+"\n"
+print(finalTransmit)
+actualString= "7E 00 19 10 01 00 00 00 00 00 00 FF FF FF FF 00 00 48 65 6C 6C 6F 20 57 6F 72 6C 64 D6"
 
 with ps.Serial(com, constants.baudrate,timeout=constants.timeout) as ser:
     while True:
-       ser.write(bytearray.fromhex(finalTransmit))
+       x=ser.write(bytes.fromhex(finalTransmit))#bytearray.fromhex(finalTransmit))
 ser.close()
 
 
