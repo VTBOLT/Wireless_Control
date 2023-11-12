f391b1c7a55ab22daa9c3657b0e6c8cf06d9b92e
Rumi Khamidov <rkham@vt.edu>
2023-11-11 20:47:09 -0500

    Rumi's work

--- recieve.py
+++ recieve.py
@@ -2,15 +2,25 @@ import time
 import math
 import serial as ps
 import constants
+import codecs
+import binascii
+import string
 constants = constants.Constants # Check the constants as needed for testing
 
 # User settings
 com = 'COM3'
 
 
-# Object instatiation
+# Reads the frame recieved by the Xbee
 with ps.Serial(com, constants.baudRate, constants.timeout) as ser:
-    while True:
-        x=ser.readline()
-        print(x)
-        print()
+    run = True
+    while run:
+        rawFile = ser.readline()
+        
+        printable = set(string.printable)
+        ''.join(filter(lambda x: x in printable, rawfile))
+
+        asciiConvert = bytes.fromhex(rawFile).decode("ASCII")
+        trimmedMsg = asciiConvert.replace(';', '\n- ')
+        print(timmedMsg)
+        run = False
