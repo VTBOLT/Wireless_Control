2aa2be70e508bcc1c51f2e415cddb64f40dd7b78
Rumi Khamidov <rkham@vt.edu>
2023-11-20 20:42:38 -0500

    Recieve Data method
    converts hex into ascii

--- recieve.py
+++ recieve.py
@@ -5,22 +5,29 @@ import constants
 import codecs
 import binascii
 import string
-constants = constants.Constants # Check the constants as needed for testing
 
 # User settings
 com = 'COM3'
-
+constants = constants.Constants # Check the constants as needed for testing
 
 # Reads the frame recieved by the Xbee
-with ps.Serial(com, constants.baudRate, constants.timeout) as ser:
-    run = True
-    while run:
-        rawFile = ser.readline()
-        
+def recieveData(message):
+    #with ps.Serial(com, constants.baudRate, constants.timeout) as ser:
+        #rawFile = ser.readline()
+        #dataList = message.split(" ")
         printable = set(string.printable)
-        ''.join(filter(lambda x: x in printable, rawfile))
+        message = message.replace(' ','').lower()
+        message = ''.join(filter(lambda x: x in printable, message))
+        
+        startDelim = message[:2]# dataList[0]
+        checkSum = message[-2:]
+        data = message[34:len(message)-2]
 
-        asciiConvert = bytes.fromhex(rawFile).decode("ASCII")
-        trimmedMsg = asciiConvert.replace(';', '\n- ')
-        print(timmedMsg)
-        run = False
+        asciiConvert = bytes.fromhex(data).decode('ascii')
+        asciiMsg = asciiConvert.replace(';', '\n')
+
+        return asciiMsg
+
+# Message is: "hëllo world;test!" -> output should be: "hllo world
+#                                                       test!"
+print(recieveData('7E 00 13 10 01 00 00 00 00 00 00 FF FF FF FF 00 00 68 65 6C 6C 6F 20 77 6F 72 6C 64 3B 74 65 73 74 21 01'))
