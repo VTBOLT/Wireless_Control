4b01dc621514217423970168818bea693862563f
aaronkanefsky <aaronk03@vt.edu>
2024-01-18 13:33:47 -0500

    tests file will contain the serial test to ensure that the serial monitor is seen through the code

--- tests.py
+++ tests.py
@@ -1,4 +1,12 @@
-# Test file to check if we are getting the right data and sending the right data
-import os
-
-from flask import Flask
+import serial
+ser = serial.Serial(port="COM4")
+received_hex_data = ""
+bytesToRead = 3
+# Example usage
+serial_port = "COM4"  # Replace with your actual serial port
+while 1:
+    inWaiting = ser.in_waiting
+    if inWaiting >= bytesToRead:
+        received_hex_data = ser.read(bytesToRead).hex()
+    
+    print( received_hex_data)
